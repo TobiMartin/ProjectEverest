0cf0ee093fa19aa9897d8a0e083a03e90c25e98d
Lin Xu <lin.xu@uni-ulm.de>
2016-12-13 16:26:42 +0100

    add calendar dialogs to start date and dead line in AddProject Activity

--- app/src/main/java/com/linxu/mounteverest/AddProject.java
+++ app/src/main/java/com/linxu/mounteverest/AddProject.java
@@ -1,16 +1,22 @@
 package com.linxu.mounteverest;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by lin xu on 02.12.2016.
@@ -23,7 +29,13 @@ public class AddProject extends AppCompatActivity {
     private EditText learningStepPercent3;
     private TableLayout tableLayout;
     private TableRow learningStepRow;
-    private TextView deadLine;
+    private EditText startDate;
+    private EditText deadLine;
+    private Calendar start_myCalendar;
+    private Calendar end_myCalendar;
+    private DatePickerDialog.OnDateSetListener start_date;
+    private DatePickerDialog.OnDateSetListener end_date;
+
     int n = 4;
 
     @Override
@@ -36,7 +48,44 @@ public class AddProject extends AppCompatActivity {
 
         tableLayout = (TableLayout) findViewById(R.id.learningStepTable);
 
-        deadLine = (TextView)findViewById(R.id.deadLine);
+        startDate = (EditText)findViewById(R.id.startDate);
+        deadLine = (EditText)findViewById(R.id.deadLine);
+
+        start_myCalendar = Calendar.getInstance();
+        start_date = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                start_myCalendar.set(Calendar.YEAR, year);
+                start_myCalendar.set(Calendar.MONTH, month);
+                start_myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        end_myCalendar = Calendar.getInstance();
+        end_date = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                end_myCalendar.set(Calendar.YEAR, year);
+                end_myCalendar.set(Calendar.MONTH, month);
+                end_myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(AddProject.this, start_date, start_myCalendar.get(Calendar.YEAR), start_myCalendar.get(Calendar.MONTH), start_myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        deadLine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(AddProject.this, end_date, end_myCalendar.get(Calendar.YEAR), start_myCalendar.get(Calendar.MONTH), end_myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         save = (Button)findViewById(R.id.save);
         save.setOnClickListener(new View.OnClickListener() {
@@ -84,5 +133,18 @@ public class AddProject extends AppCompatActivity {
         intent.putExtra("learningStepPercent2", learningStepPercent2.getText().toString());
         intent.putExtra("learningStepPercent3", learningStepPercent3.getText().toString());
         startActivity(intent);
+        formValidate();
+    }
+
+    private void
+        formValidate() {
+        //Todo: to check if all the blanks are filled with proper data
+    }
+
+    private void updateLabel(){
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        startDate.setText(sdf.format(start_myCalendar.getTime()));
+        deadLine.setText(sdf.format(end_myCalendar.getTime()));
     }
 }
